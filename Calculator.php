8544c6f7eb5af81ec9ba734d0930c0cc58a21838
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2017-12-22 12:25:28 +0100

    Final design pattern

--- Calculator.php
+++ Calculator.php
@@ -12,26 +12,26 @@ class Calculator
             echo "Command available :\n1:\tAdd\n2:\tMulti\n3:\tQuit\n";
             $line = readline("> ");
 
-            switch ($line) {
-                case "1":
-                    $a = readline("> a: ");
-                    $b = readline("> b: ");
-                    $com = new AddCommand($a);
-                    $c = $com->do($b);
-                    echo "c: $c\n";
-                    break;
-                case "2":
-                    $a = readline("> a: ");
-                    $b = readline("> b: ");
-                    $com = new MultiCommand($a);
-                    $c = $com->do($b);
-                    echo "c: $c\n";
-                    break;
-                case 3:
-                    break;
-                default:
-                    echo "Command not found ! Try again...\n";
+            if ($line == "1" || $line == "2") {
+                $a = readline("> a: ");
+                $b = readline("> b: ");
+
+                switch ($line) {
+                    case "1":
+                        $com = new AddCommand($a);
+                        break;
+                    case "2":
+                        $com = new MultiCommand($a);
+                        break;
+                    default:
+                }
+                $c = $com->do($b);
+                echo "c: $c\n";
+
+            } else if ($line != "3") {
+                echo "Command not found ! Try again...\n";
             }
+
             echo "\n";
 
         }
